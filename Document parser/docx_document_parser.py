4514372d6f9b3020ec4d1a99f456da00dc0d3e5c
AncaGrigoriu <grigoriu.anca2012@gmail.com>
2017-12-11 17:20:55 +0200

    doc, docx image info

--- Document parser/docx_document_parser.py
+++ Document parser/docx_document_parser.py
@@ -1,45 +1,52 @@
-import zipfile
-import os
-import sys
+import zipfile, os, sys, json, shutil
 from win32com import client
 from os import path
 import xml.etree.ElementTree as ET
-import re
+import ImageSerialization
 
 def getInfo(filepath):
+    parentDir = os.path.dirname(filepath)
     dictRels = dict()
-    relsFilePath = os.path.join(filepath, "word", "_rels", "document.xml.rels")
+
+    relsFilePath = os.path.join(parentDir, "word", "_rels", "document.xml.rels")
     tree = ET.parse(relsFilePath)
     root = tree.getroot()
     for child in root:
         if 'image' in child.attrib['Type']:
             dictRels[child.attrib['Id']] = child.attrib['Target']
-    print(dictRels)
 
-    dictFinal = dict()
+    finalArray = []
 
-    docFilePath = os.path.join(filepath, "word", "document.xml")
+    docFilePath = os.path.join(parentDir, "word", "document.xml")
 
     namespaces = {'wp': 'http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing',
                   'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main',
                   'a': 'http://schemas.openxmlformats.org/drawingml/2006/main',
                   'pic': 'http://schemas.openxmlformats.org/drawingml/2006/picture'}
+
     tree = ET.parse(docFilePath)
     root = tree.getroot()
 
     for e in root.findall('.//w:drawing/wp:anchor/a:graphic/a:graphicData/pic:pic', namespaces):
-        dictNou = dict()
-        for item in e.findall('.//pic:nvPicPr/pic:cNvPr', namespaces):
-            if 'descr' in item.attrib:
-                dictNou['caption'] = item.attrib['descr']
         for item in e.findall('.//pic:blipFill/a:blip', namespaces):
             for key, value in item.attrib.items():
                 if 'embed' in key:
-                    dictNou['id'] = value
+                    image = value
                     break
-        dictFinal[]
 
-    return (dictNou)
+        newpath = os.path.join('.', 'word', dictRels[image])
+        dictNou = ImageSerialization.get_info_from_image(newpath)
+
+        for item in e.findall('.//pic:nvPicPr/pic:cNvPr', namespaces):
+            if 'descr' in item.attrib:
+                dictNou['caption'] = item.attrib['descr']
+
+        dictNou['document'] = os.path.basename(filepath)
+
+        finalArray.append(dictNou)
+
+    return finalArray
+
 
 def convert(filepath):
     try:
@@ -66,18 +73,26 @@ def extractImages(filepath):
     z = zipfile.ZipFile(filepath)
     all_files = z.namelist()
 
-    images = filter(lambda f : f.startswith('word/media'), all_files)
+    images = filter(lambda f : f.startswith('word'), all_files)
 
     for item in images:
         z.extract(item, r'.')
 
-if len(sys.argv) != 2:
-    print("Wrong number of parameters")
-    exit(0)
+    result = getInfo(filepath)
+    shutil.rmtree('./word')
 
-filepath = sys.argv[1]
+    with open('result.json', 'w') as output:
+        output.write(json.dumps(result, separators=(',', ':')) + '\n')
 
-if filepath.endswith(".doc"):
-    convert(filepath)
-else:
-    extractImages(filepath)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Wrong number of parameters")
+        exit(0)
+
+    filepath = sys.argv[1]
+
+    if filepath.endswith(".doc"):
+        convert(filepath)
+    else:
+        extractImages(filepath)
