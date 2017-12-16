28ac355a165fc23378d3a00e9b8dacc65fccd976
StefanCosminR <stefancosmin.romanescu@gmail.com>
2017-12-16 16:16:27 +0200

    Serialization of folder is now independent of folder size

--- DocumentParser/ImageSerialization.py
+++ DocumentParser/ImageSerialization.py
@@ -41,19 +41,29 @@ if __name__ == '__main__':
     if not os.path.isdir(sys.argv[1]):
         raise ValueError('The path does not point to a directory but to other type of file')
 
-    result = []
+    with open('result.json', 'w') as output:
+        output.write('[')
 
-    for files in os.listdir(sys.argv[1]):
-        file_path = os.path.join(sys.argv[1], files)
-        if os.path.isfile(file_path):
-            if file_path.lower().endswith(('.png', '.jpg', '.jpeg')):
-                result.append(get_info_from_image(file_path))
-                print(f'>> finished analyzing file: {file_path}')
+        first = True
+        for files in os.listdir(sys.argv[1]):
+            file_path = os.path.join(sys.argv[1], files)
+            if os.path.isfile(file_path):
+                if file_path.lower().endswith(('.png', '.jpg', '.jpeg')):
+                    result = get_info_from_image(file_path)
+                    print(f'>> finished analyzing file: {file_path}; started writing')
+                    if first:
+                        first = False
+                        output.write(json.dumps(result, separators=(',', ':')))
+                    else:
+                        output.write(',' + json.dumps(result, separators=(',', ':')))
+                    print(f'>> finished writing output')
+
+        output.write(']')
 
     print('>> done parsing files')
-    print('>> starting writing to file')
-
-    with open('result.json', 'w') as output:
-        output.write(json.dumps(result, separators=(',', ':')) + '\n')
+    # print('>> starting writing to file')
+    #
+    # with open('result.json', 'w') as output:
+    #     output.write(json.dumps(result, separators=(',', ':')) + '\n')
 
     print('>> finished writing results to file')
