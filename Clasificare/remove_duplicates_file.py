5527d1ca5965c36c226a669a84cf62173b42ad89
CatalinPJ <CatalinPJ@users.noreply.github.com>
2017-12-18 00:45:28 +0200

    Remove duplicate images from training set

---
+++ Clasificare/remove_duplicates_file.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+
+def relative_files(path):
+    dic = {}
+    for root, dirnames, files in os.walk(path):
+        relroot = os.path.relpath(root, path)
+        for filename in files:
+            if filename not in dic:
+                dic[filename] = []
+            dic[filename].append(os.path.join(root, filename))
+    for key in dic:
+        if len(dic[key]) > 1:
+            for path in dic[key]:
+                os.remove(path)
+
+
+if __name__ == "__main__":
+    directory = sys.argv[1]
+    relative_files(directory)
