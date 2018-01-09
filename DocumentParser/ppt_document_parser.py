04606d50c8324f76b37aa91a45e08abbd4670c6c
Unknown <anghelinaelena96@gmail.com>
2018-01-09 12:29:52 +0200

    save photos to dir result + db

--- DocumentParser/ppt_document_parser.py
+++ DocumentParser/ppt_document_parser.py
@@ -4,8 +4,10 @@ import json
 import sys
 import win32com
 import pptx
+import shutil
 from pptx import Presentation
 import ImageSerialization
+import DatabaseConnection
 
 def PPT_extract_caption(filepath):
 
@@ -20,6 +22,25 @@ def PPT_extract_caption(filepath):
                     titles.append(os.path.splitext(os.path.basename(filepath))[0])
 
     return titles
+
+def saveImage(path, picName):
+
+    pic = os.path.basename(picName)
+
+    if not os.path.exists("result"):
+        os.mkdir("result")
+
+    newpath = os.path.join("result", pic)
+
+    fout = open(newpath, 'w')
+    fout.close()
+
+    with open(path, 'rb') as fin:
+        with open(newpath, 'ab') as fout:
+            buff = fin.read(128)
+            while buff:
+                fout.write(buff)
+                buff = fin.read(128)
 
 def PPT_convertor(filepath):
     try:
@@ -57,19 +78,32 @@ def PPTX_extractImages(filepath):
             z.extract(img, r'.')
             contor += 1
             result_json = ImageSerialization.get_info_from_image(img)
+
+            newpath = os.path.join('.', img)
+            saveImage(newpath,img)
+
             if contor < len(caption):
                 result_json['caption'] = caption[contor]
             else:
                 result_json['caption'] = os.path.splitext(os.path.basename(filepath))[0]
+
+            del result_json['pixels']
+            result_json['path'] = os.path.join('.','result', os.path.basename(img))
             finalResult.append(result_json)
             contor += 1
 
         file = open("%sresult.json" % argv[0], 'w')
         json.dump(finalResult, file, separators=(',', ':'))
+
+        db = DatabaseConnection.DatabaseConnection()
+        db.insert_entry(finalResult)
+
         file.close()
+        shutil.rmtree('./ppt')
 
 
 def ppt_document_parser(argv):
+
     if len(argv) != 2:
         print("Wrong number of parameters")
         exit(0)
